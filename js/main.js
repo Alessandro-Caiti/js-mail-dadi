89f5a796fab8081148aff33184ae01c5e3da4708
Alessandro Caiti <acaiti@hotmail.it>
2020-02-19 19:33:55 +0100

    Una modifica che penso sia corretta

--- js/main.js
+++ js/main.js
@@ -6,28 +6,28 @@
 //
 // for (var i = 0; i < partecipanti.length; i++) {
 //     if (partecipanti[i] == persona) {
-//         var accettato = true;
+//      var accettato = true;
+//     } if (accettato) {
+//         document.getElementById('risultato').innerHTML = "Puoi entrare " + persona;
+//     } else {
+//      document.getElementById('risultato').innerHTML = "Non puoi entrare " + persona;
 //     }
 // }
-//
-// if (accettato) {
-//     document.getElementById('risultato').innerHTML = "Puoi entrare " + persona;
-// } else {
-//     document.getElementById('risultato').innerHTML = "Non puoi entrare " + persona;
-// }
+
+
 
 
 // Esercizio dadi
 
-var mago = Math.floor(Math.random() * 10) + 1;
 var guerriero = Math.floor(Math.random() * 10) + 1;
+var mago = Math.floor(Math.random() * 10) + 1;
 document.getElementById('risultato').innerHTML = "Il mago ha tirato: " + mago;
 document.getElementById('risultato-2').innerHTML = "Il guerriero ha tirato: " + guerriero;
 
 if (mago > guerriero) {
     document.getElementById('vincitore').innerHTML = "Il mago ha sconfitto il guerriero";
 } else if (mago == guerriero) {
-    document.getElementById('vincitore').innerHTML = "Il mago e il guerriero sono alla pari";
+     document.getElementById('vincitore').innerHTML = "Il mago e il guerriero sono alla pari";
 } else {
-    document.getElementById('vincitore').innerHTML = "Il guerriero ha sconfitto il mago"
+     document.getElementById('vincitore').innerHTML = "Il guerriero ha sconfitto il mago"
 }

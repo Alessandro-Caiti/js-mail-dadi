313e762d7ff30e4a3edeaacaca4c1776891ec638
Alessandro Caiti <acaiti@hotmail.it>
2020-02-20 10:47:52 +0100

    Corretto come da lezione

--- js/main.js
+++ js/main.js
@@ -1,33 +1,35 @@
 // Esercizio mail
 
-// var partecipanti = ["tizio@mail.com", "caio@mail.com", "sempronio@mail.com", "pippo@mail.com", "pluto@mail.com", "paperino@mail.com"];
-// var persona = prompt("Inserisci la tua e-mail per partecipare");
-// var accettato = false;
-//
-// for (var i = 0; i < partecipanti.length; i++) {
-//     if (partecipanti[i] == persona) {
-//      var accettato = true;
-//     } if (accettato) {
-//         document.getElementById('risultato').innerHTML = "Puoi entrare " + persona;
-//     } else {
-//      document.getElementById('risultato').innerHTML = "Non puoi entrare " + persona;
-//     }
-// }
+var partecipanti = ["tizio@mail.com", "caio@mail.com", "sempronio@mail.com", "pippo@mail.com", "pluto@mail.com", "paperino@mail.com"];
+var persona = prompt("Inserisci la tua e-mail per partecipare");
+var accettato = false;
+
+for (var i = 0; i < partecipanti.length; i++) {
+    if (partecipanti[i] == persona) {
+        accettato = true;
+    }
+}
+
+if (accettato) {
+    document.getElementById('risultato').innerHTML = "Puoi entrare " + persona;
+} else {
+    document.getElementById('risultato').innerHTML = "Non puoi entrare " + persona;
+}
 
 
 
 
 // Esercizio dadi
-
-var guerriero = Math.floor(Math.random() * 10) + 1;
-var mago = Math.floor(Math.random() * 10) + 1;
-document.getElementById('risultato').innerHTML = "Il mago ha tirato: " + mago;
-document.getElementById('risultato-2').innerHTML = "Il guerriero ha tirato: " + guerriero;
-
-if (mago > guerriero) {
-    document.getElementById('vincitore').innerHTML = "Il mago ha sconfitto il guerriero";
-} else if (mago == guerriero) {
-     document.getElementById('vincitore').innerHTML = "Il mago e il guerriero sono alla pari";
-} else {
-     document.getElementById('vincitore').innerHTML = "Il guerriero ha sconfitto il mago"
-}
+//
+// var guerriero = Math.floor(Math.random() * 10) + 1;
+// var mago = Math.floor(Math.random() * 10) + 1;
+// document.getElementById('risultato').innerHTML = "Il mago ha tirato: " + mago;
+// document.getElementById('risultato-2').innerHTML = "Il guerriero ha tirato: " + guerriero;
+//
+// if (mago > guerriero) {
+//     document.getElementById('vincitore').innerHTML = "Il mago ha sconfitto il guerriero";
+// } else if (mago == guerriero) {
+//      document.getElementById('vincitore').innerHTML = "Il mago e il guerriero sono alla pari";
+// } else {
+//      document.getElementById('vincitore').innerHTML = "Il guerriero ha sconfitto il mago"
+// }
